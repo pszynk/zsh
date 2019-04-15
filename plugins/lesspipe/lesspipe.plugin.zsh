6ca49b587ec5e6d4b5b1062f8a9b098bf762f02d
Paweł Szynkiewicz <sed s/x/pszynk/  x@gmail.com>
2019-04-15 16:27:13 +0200

    lesspipe with guards and instruction for termianl colors...

--- plugins/lesspipe/lesspipe.plugin.zsh
+++ plugins/lesspipe/lesspipe.plugin.zsh
@@ -3,6 +3,20 @@
 #   lesspipe.sh - https://github.com/wofr06/lesspipe
 #   pygments / pygmentize - http://pygments.org/docs/cmdline/ (pip pygments, pac pygmentize)
 
-export LESSOPEN="|lesspipe.sh %s"
 export LESS="-R"
-export LESSCOLORIZER="pygmentize -f terminal"
+if type lesspipe.sh >/dev/null 2>&1; then
+    export LESSOPEN="|lesspipe.sh %s"
+fi
+
+# modify code2color file from package lesspipe,
+# not the most elegant of solutions...
+#
+# <<<
+# -system($ENV{LESSCOLORIZER}, @ARGV);
+# ===
+# +system($ENV{LESSCOLORIZER}, '-f', 'terminal', @ARGV);
+# >>>
+#
+if type pygmentize >/dev/null 2>&1; then
+    export LESSCOLORIZER="pygmentize"
+fi
